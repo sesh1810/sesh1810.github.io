1ecaaa7607dc27b67856a4ae48bd59ffec0b42b2
Seshasayi Rangaraj <52456289+sesh1810@users.noreply.github.com>
2026-07-27 23:20:37 -0700

    Add files via upload

---
+++ p2.js
@@ -0,0 +1,1 @@
+document.getElementById('out').insertAdjacentHTML('beforeend','<li>SLOT 2 — small external file: RAN</li>');
---
+++ p5.js
@@ -0,0 +1,1 @@
+document.getElementById('out').insertAdjacentHTML('beforeend','<li>SLOT 5 — last script tag on page: RAN</li>');
